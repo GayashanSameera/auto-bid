8d83dbea00ee9f1eb016ebc27744b00cb2036a61
GayashanSameera <gayashansameera30@gmail.com>
2022-10-14 11:50:45 +0530

    login modal

--- components/modals/loginModal.js
+++ components/modals/loginModal.js
@@ -1,23 +1,60 @@
-import { Modal, Row, Col } from 'antd';
+import { Modal, Row, Button } from 'antd';
+import { Field, Formik, Form } from "formik";
+
+import { InputField } from '../formFields';
+
+import styles from './loginModal.module.css';
 
 export const LoginModal = (props) => {
     const { show, closeLoginModal } = props;
+
     return (
         <Modal
             visible={show}
             footer={null}
             maskClosable={false}
             onCancel={closeLoginModal}
+            className={styles.loginModal}
         >
             <div>
                 <Row>
-                    <h1>Login</h1>
+                    <h1 className={styles.loginHeader}>Login</h1>
                 </Row>
                 <Row>
-                    <p>If you have an account, sign in with your email address.</p>
+                    <span>If you have an account, sign in with your email address.</span>
                 </Row>
+                <Formik
+                    initialValues={{ email: null, password: null }}
+                    onSubmit={(values, actions) => {
+                        console.log('values', values);
+                    }}
+                >
+                    {(props) => (
+                        <Form>
+                            <Row className={styles.labelRow}>
+                                <label className={styles.loginLabel}>Email Address</label>
+                            </Row>
+                            <Row>
+                                <Field component={InputField} type="email" name="email" placeholder="Enter Email Address" />
+                            </Row>
+                            <Row className={styles.labelRow}>
+                                <label className={styles.loginLabel}>Password</label>
+                            </Row>
+                            <Row>
+                                <Field name="password" placeholder="Enter Password" component={InputField} />
+                            </Row>
+
+                            <Row className={styles.labelRow}>
+                                <Button type="primary" htmlType="submit">
+                                    Submit
+                                </Button>
+                            </Row>
+                        </Form>
+                    )}
+                </Formik>
             </div>
         </Modal>
     );
 
 };
+
